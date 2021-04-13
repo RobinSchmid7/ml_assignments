1e02966374998a8c8985f72b965a36098ae58e7e
Robin Schmid <schmirob@student.ethz.ch>
2021-04-13 21:58:27 +0200

    Progress, SVC not working yet

--- task2/Robin/main.py
+++ task2/Robin/main.py
@@ -7,6 +7,9 @@ Apr, 2021
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import NuSVC
+from sklearn.svm import NuSVR
 
 df_train_features = pd.read_csv("../handout/train_features.csv")
 df_train_labels = pd.read_csv("../handout/train_labels.csv")
@@ -49,6 +52,8 @@ regression_features = [r[6:] for r in regression_labels]
 # In first step do not use other features, later try to use features with a lot of missing data and use the aveage over all time steps for these features.
 # Try to explore if using a mask helps - a binary signal which tell if the given feature has a nan value or not
 
+### Preprocessing training set
+
 # Only use features of data if the data for this feature is available more than a certain threshold
 num_data = len(df_train_features)
 
@@ -57,7 +62,7 @@ percentage_available = np.sum(~df_train_features.isnull()) / num_data
 
 # Reduced feature set
 df_train_features = df_train_features[percentage_available[percentage_available > available_threshold].index.tolist()]
-# Do not use Age as a feature where time matters
+# Do not use Age as a feature where time matters //TODO: remove more elegantly
 df_train_features = df_train_features.drop(columns="Age")
 # Remaining features: Age, RRate, ABPm, SpO2, Heartrate, ABPs
 
@@ -68,12 +73,68 @@ df_train_features.index = pd.MultiIndex.from_arrays([df_train_features.index.get
 # Unstack features with few missing data to account for the time they were took
 df_train_features = df_train_features.unstack(level=-1)
 
-# Interpolate missing values //TODO: speed this up (openmp for python?)
-print("Start interpolating...")
-for i in range(len(df_train_features.columns)):
-    df_train_features[df_train_features.columns[i:i+12]] = df_train_features[df_train_features.columns[i:i+12]].interpolate(axis=1)
-print("Interpolating finished")
+# Interpolate missing values
+#TODO: check missing values in pre processed data table - some are not interpolated? what is the problem for this??
+#TODO: speed this up (openmp for python?)
 
-np.savetxt("filtered_train_features.csv",df_train_features,comments='',delimiter=",",fmt="%s")
+# print("Start interpolating...")
+# for i in range(len(df_train_features.columns)):
+#     df_train_features[df_train_features.columns[i:i+12]] = df_train_features[df_train_features.columns[i:i+12]].interpolate(axis=1)
+# print("Interpolating finished")
+#
+# # Load saved interpolated data for quicker debugging
+# TODO: remove this in the end
+
+# np.savetxt("filtered_train_features.csv",df_train_features,comments='',delimiter=",",fmt="%s")
+
+# For debugging load data since interpolating takes some time
+df_train_features = pd.read_csv("filtered_train_features.csv")
+
+### Preprocessing test set
+
+num_data = len(df_test_features)
+
+# Percentage of data available of each feature
+percentage_available = np.sum(~df_test_features.isnull()) / num_data
+
+# Reduced feature set
+df_test_features = df_test_features[percentage_available[percentage_available > available_threshold].index.tolist()]
+# Do not use Age as a feature where time matters //TODO: remove more elegantly
+df_test_features = df_test_features.drop(columns="Age")
+# Remaining features: Age, RRate, ABPm, SpO2, Heartrate, ABPs
+
+# Shift data such that the time series goes from 0 to 11
+df_test_features.index = pd.MultiIndex.from_arrays([df_test_features.index.get_level_values(0),
+                                                     df_test_features.groupby(level=0).cumcount()],
+                                                    names=["pid","Time"])
+# Unstack features with few missing data to account for the time they were took
+df_test_features = df_test_features.unstack(level=-1)
+
+# Interpolate missing values
+#TODO: check missing values in pre processed data table - some are not interpolated? what is the problem for this??
+#TODO: speed this up (openmp for python?)
+
+# print("Start interpolating...")
+# for i in range(len(df_test_features.columns)):
+#     df_test_features[df_test_features.columns[i:i+12]] = df_test_features[df_test_features.columns[i:i+12]].interpolate(axis=1)
+# print("Interpolating finished")
+
+# Load saved interpolated data for quicker debugging
+# TODO: remove this in the end
+
+# np.savetxt("filtered_test_features.csv",df_test_features,comments='',delimiter=",",fmt="%s")
+
+# For debugging load data since interpolating takes some time
+df_test_features = pd.read_csv("filtered_train_features.csv")
+
+# Scale data, equalizes one sided distribution
+scaler = StandardScaler()
+scaler.transform(df_train_features)
+
+# Try using SVM for classification
+clf = NuSVC(kernel='linear')
+clf.fit(df_train_features,df_train_labels)
+
+clf.predict(df_test_features)
 
 pass
