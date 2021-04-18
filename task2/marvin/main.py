6500f3d34f723ad6afaadb4b496c50a31fa84e5b
harmsm <harmsm@student.ethz.ch>
2021-04-18 19:58:05 +0200

    changed task1 to independent SVMs

--- task2/marvin/main.py
+++ task2/marvin/main.py
@@ -11,34 +11,32 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.impute import KNNImputer
 from sklearn.pipeline import make_pipeline
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.svm import SVR
 
-df_train_features = pd.read_csv("../handout/train_features.csv")
-df_train_labels = pd.read_csv("../handout/train_labels.csv")
-df_test_features = pd.read_csv("../handout/test_features.csv")
+def softmax(x):
+    '''return the softmax of a vector x'''
+    return 1/(1+np.exp(x))
 
-# ================================
-# load data into pandas dataframes
-# ================================
-df_train_features = df_train_features.set_index(["pid","Time"]).sort_index()
-df_train_labels = df_train_labels.set_index(["pid"]).sort_index()
-df_test_features = df_test_features.set_index(["pid","Time"]).sort_index()
-
+############
+# USER INPUT
+############
+use_preprocessed = False
 available_threshold = 0.5
-
+kernel= 'linear'
 
 # get headers for Task 1
 classification_labels = [
-    "LABEL_BaseExcess",
-    "LABEL_Fibrinogen",
-    "LABEL_AST",
-    "LABEL_Alkalinephos",
-    "LABEL_Bilirubin_total",
-    "LABEL_Lactate",
-    "LABEL_TroponinI",
-    "LABEL_SaO2",
-    "LABEL_Bilirubin_direct",
-    "LABEL_EtCO2"
+"LABEL_BaseExcess",
+"LABEL_Fibrinogen",
+"LABEL_AST",
+"LABEL_Alkalinephos",
+"LABEL_Bilirubin_total",
+"LABEL_Lactate",
+"LABEL_TroponinI",
+"LABEL_SaO2",
+"LABEL_Bilirubin_direct",
+"LABEL_EtCO2"
 ]
 classification_features = [c[6:] for c in classification_labels]
 
@@ -48,51 +46,101 @@ sepsis_features = [s[6:] for s in sepsis_label]
 
 # get headers for Task 3
 regression_labels = [
-    "LABEL_RRate", "LABEL_ABPm", "LABEL_SpO2", "LABEL_Heartrate"
+"LABEL_RRate", "LABEL_ABPm", "LABEL_SpO2", "LABEL_Heartrate"
 ]
 regression_features = [r[6:] for r in regression_labels]
 
-# Idea for pre-processing:
-# First, for each patient, flatten the data rows into one row, by taking the average of all non-nan measurements.
-# Then, each dimension of the training data is normalized using standard scaler. For each missing measurement,
-# we run a kNN classifier, copying the average of the k nearest neighbors to the missing value. 
-# Closeness is evaluated based on frequent features as:
-# Age, RRate, ABPm, SpO2, HR, ABPs
+if not use_preprocessed:
+    df_train_features = pd.read_csv("../handout/train_features.csv")
+    df_train_labels = pd.read_csv("../handout/train_labels.csv")
+    df_test_features = pd.read_csv("../handout/test_features.csv")
 
-# ==========================
-# Preprocessing training set
-# ==========================
-# eliminate time dependency
-df_train_features = df_train_features.groupby('pid').mean()
+    # ================================
+    # load data into pandas dataframes
+    # ================================
+    df_train_features = df_train_features.set_index(["pid"]).sort_index()
+    df_train_labels = df_train_labels.set_index(["pid"]).sort_index()
+    df_test_features = df_test_features.set_index(["pid"])
 
-# eliminate features, that are missing with more than 50% of the patients
-num_features = len(df_train_features)
-percentage_available = np.sum(~df_train_features.isnull()) / num_features
-df_train_features = df_train_features[percentage_available[percentage_available > available_threshold].index.tolist()]
+    # Idea for pre-processing:
+    # First, for each patient, flatten the data rows into one row, by taking the average of all non-nan measurements.
+    # Then, we run a kNN classifier, copying the average of the k nearest neighbors to the missing value. 
+    # Closeness is evaluated based on all other, non-nan features
 
-# TODO: remove later
-print(df_train_features.columns)
-print(df_train_features.shape)
-print(df_train_labels.shape)
+    # ==========================
+    # Preprocessing training set
+    # ==========================
+    # eliminate time dependency
+    df_train_features.drop('Time',axis=1)
+    df_train_features = df_train_features.groupby('pid',sort=False).mean()
 
-# apply standard scaler
-feature_scaler = StandardScaler()
-label_scaler = StandardScaler()
-X_train = feature_scaler.fit_transform(df_train_features.values)
-Y_train = label_scaler.fit_transform(df_train_labels.values)
+    # eliminate features, that are missing with more than 50% of the patients
+    num_features = len(df_train_features)
+    percentage_available = np.sum(~df_train_features.isnull()) / num_features
+    df_train_features = df_train_features[percentage_available[percentage_available > available_threshold].index.tolist()]
 
-# replace missing values by KNN-imputation
-imputer = KNNImputer(n_neighbors=3)
-X_train = imputer.fit_transform(X_train)
+    # apply standard scaler
+    feature_scaler = StandardScaler()
+    X_train = feature_scaler.fit_transform(df_train_features.values)
 
-# =========================
-# Preprocessing of test set
-# =========================
-# TODO: add processing steps, identical to training set
+    # replace missing values by KNN-imputation
+    imputer = KNNImputer(n_neighbors=3)
+    X_train = imputer.fit_transform(X_train)
+    df_train_features[df_train_features.columns] = X_train
+
+
+    # =========================
+    # Preprocessing of test set
+    # =========================
+    # eliminate time dependency
+    df_test_features.drop('Time',axis=1)
+    df_test_features = df_test_features.groupby('pid',sort=False).mean()
+
+    # eliminate features, that are missing with more than 50% of the patients
+    df_test_features = df_test_features[percentage_available[percentage_available > available_threshold].index.tolist()]
+
+    # apply standard scaler
+    X_test = feature_scaler.transform(df_test_features.values)
+
+    # replace missing values by KNN-imputation
+    X_test = imputer.transform(X_test)
+    df_test_features[df_test_features.columns] = X_test
+
+    # Intermittent step: save the preprocessed data
+    df_train_features.to_csv('df_train_features.csv')
+    df_train_labels.to_csv('df_train_labels.csv')
+    df_test_features.to_csv('df_test_features.csv')
+
+else:
+    df_train_features = pd.read_csv('df_train_features.csv')
+    df_train_labels = pd.read_csv('df_train_labels.csv')
+    df_test_features = pd.read_csv('df_test_features.csv')
+
 
 # ============================
 # Task 1: predict medical test
 # ============================
+df_test_labels = pd.DataFrame(index=df_test_features.index)
+df_test_labels.index.names = ['pid']
+svm = LinearSVC(max_iter=10000,dual=False)
+# we let the linear svm fit and predict each test individually:
+for label in classification_labels:
+    svm.fit(df_train_features.to_numpy(),np.round(df_train_labels[label].to_numpy()))
+    # compute distance to hyperplane
+    distance = svm.decision_function(df_test_features.to_numpy())/np.linalg.norm(svm.coef_)
+    # the predictor is the softmax of the negative distance:
+    pred = softmax(-1*distance)
+    df_test_labels[label] = pred
+
+# suppose df is a pandas dataframe containing the result
+df_test_labels.to_csv('prediction.csv', index=True, float_format='%.3f')
+# try to evaluate the performance:
+# we do 5-fold cross-validation:
+
+# TODO: include 5-fold CV test
 
 
+# ======================
+# Task 2: predict sepsis
+# ======================
 
