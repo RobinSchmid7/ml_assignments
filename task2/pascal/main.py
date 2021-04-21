f03912b354c3e91404937624c22b65783fa31022
PMU <pascal.mueller@thurweb.ch>
2021-04-21 12:05:21 +0200

    added inplace to remove time

--- task2/pascal/main.py
+++ task2/pascal/main.py
@@ -21,12 +21,13 @@ def softmax(x):
     """ return the softmax of a vector x """
     return 1 / (1 + np.exp(x))
 
+
 ############
 # USER INPUT
 ############
 use_preprocessed = False
 available_threshold = 0.3
-kernel = 'linear'
+# kernel = 'linear'
 
 # get headers for Task 1
 classification_labels = [
@@ -67,6 +68,8 @@ if not use_preprocessed:
     df_train_labels = df_train_labels.set_index(["pid"]).sort_index()
     df_test_features = df_test_features.set_index(["pid"])
 
+    print(df_train_features)
+
     # Idea for pre-processing:
     # First, for each patient, flatten the data rows into one row, by taking the average of all non-nan measurements.
     # Then, we run a kNN classifier, copying the average of the k nearest neighbors to the missing value.
@@ -82,108 +85,110 @@ if not use_preprocessed:
     # preprocessing training set
     # ==========================
     # eliminate time dependency
-    df_train_features.drop('Time', axis=1)
-    # we average all values and append the gradient of the following columns
-    # RRate, Heartrate, ABPm, ABPd, SpO2
-    for column in ['RRate', 'Heartrate', 'ABPm', 'ABPd', 'SpO2']:
-        df_train_features[str('gradient_' + column)] = df_train_features[column].groupby('pid', sort=False).diff()
+    df_train_features.drop(labels='Time', axis=1, inplace=True)
 
-    df_train_features = df_train_features.groupby('pid', sort=False).mean()
+    # # we average all values and append the gradient of the following columns
+    # # RRate, Heartrate, ABPm, ABPd, SpO2
+    # for column in ['RRate', 'Heartrate', 'ABPm', 'ABPd', 'SpO2']:
+    #     df_train_features[str('gradient_' + column)] = df_train_features[column].groupby('pid', sort=False).diff()
+    # df_train_features = df_train_features.groupby('pid', sort=False).mean()
+    #
+    # num_features = len(df_train_features)
+    # percentage_available = np.sum(~df_train_features.isnull()) / num_features
+    # df_train_features = df_train_features[
+    #     percentage_available[percentage_available > available_threshold].index.tolist()]
+    #
+    # # apply standard scaler
+    # X_train = feature_scaler.fit_transform(df_train_features.values)
+    #
+    # # replace missing values by KNN-imputation
+    # X_train = imputer.fit_transform(X_train)
+    # df_train_features[df_train_features.columns] = X_train
 
-    # eliminate features, that are missing with more than 50% of the patients
-    num_features = len(df_train_features)
-    percentage_available = np.sum(~df_train_features.isnull()) / num_features
-    df_train_features = df_train_features[
-        percentage_available[percentage_available > available_threshold].index.tolist()]
-
-    # apply standard scaler
-    X_train = feature_scaler.fit_transform(df_train_features.values)
-
-    # replace missing values by KNN-imputation
-    X_train = imputer.fit_transform(X_train)
-    df_train_features[df_train_features.columns] = X_train
-
-    # =========================
-    # preprocessing of test set
-    # =========================
-    # eliminate time dependency
-    df_test_features.drop('Time', axis=1)
-    # we average all values and append the gradient of the following columns:
-    # RRate, Heartrate, ABPm, ABPd, SpO2
-    for column in ['RRate', 'Heartrate', 'ABPm', 'ABPd', 'SpO2']:
-        df_test_features[str('gradient_' + column)] = df_test_features[column].groupby('pid', sort=False).diff()
-    df_test_features = df_test_features.groupby('pid', sort=False).mean()
-
-    # eliminate features, that are missing with more than 50% of the patients
-    df_test_features = df_test_features[percentage_available[percentage_available > available_threshold].index.tolist()]
-
-    # apply standard scaler
-    X_test = feature_scaler.transform(df_test_features.values)
-
-    # replace missing values by KNN-imputation
-    X_test = imputer.transform(X_test)
-    df_test_features[df_test_features.columns] = X_test
-
-    # intermittent step: save the preprocessed data
+    # # =========================
+    # # preprocessing of test set
+    # # =========================
+    # # eliminate time dependency
+    # df_test_features.drop('Time', axis=1)
+    # # we average all values and append the gradient of the following columns:
+    # # RRate, Heartrate, ABPm, ABPd, SpO2
+    # for column in ['RRate', 'Heartrate', 'ABPm', 'ABPd', 'SpO2']:
+    #     df_test_features[str('gradient_' + column)] = df_test_features[column].groupby('pid', sort=False).diff()
+    # df_test_features = df_test_features.groupby('pid', sort=False).mean()
+    #
+    # # eliminate features, that are missing with more than 50% of the patients
+    # df_test_features = df_test_features[percentage_available[percentage_available > available_threshold].index.tolist()]
+    #
+    # # apply standard scaler
+    # X_test = feature_scaler.transform(df_test_features.values)
+    #
+    # # replace missing values by KNN-imputation
+    # X_test = imputer.transform(X_test)
+    # df_test_features[df_test_features.columns] = X_test
+    #
+    # # intermittent step: save the preprocessed data
     df_train_features.to_csv('df_train_features.csv')
-    df_train_labels.to_csv('df_train_labels.csv')
-    df_test_features.to_csv('df_test_features.csv')
+    # df_train_labels.to_csv('df_train_labels.csv')
+    # df_test_features.to_csv('df_test_features.csv')
+
+    print(df_train_features)
 
     print('...done')
-
 else:
+    print('Loading preprocessed data...')
     df_train_features = pd.read_csv('df_train_features.csv')
     df_train_labels = pd.read_csv('df_train_labels.csv')
     df_test_features = pd.read_csv('df_test_features.csv')
     df_train_features = df_train_features.set_index(["pid"]).sort_index()
     df_train_labels = df_train_labels.set_index(["pid"]).sort_index()
     df_test_features = df_test_features.set_index(["pid"])
+    print('...done')
 
 # ============================
 # Task 1: predict medical test
 # ============================
-df_test_labels = pd.DataFrame(index=df_test_features.index)
-df_test_labels.index.names = ['pid']
-svm = SVC(probability=True, class_weight='balanced')
-print('TASK1: predicting probabilities...')
-# we let the linear svm fit and predict each test individually
-for label in classification_labels:
-    svm.fit(df_train_features.to_numpy(), df_train_labels[label].to_numpy())
-    # compute probabilities
-    pred = svm.predict(df_test_features.to_numpy())
-    prob = svm.predict_proba(df_test_features.to_numpy())
-    df_test_labels[label] = [p[1] for p in prob]
-    print('...predicted', label)
-print('...done')
+# df_test_labels = pd.DataFrame(index=df_test_features.index)
+# df_test_labels.index.names = ['pid']
+# svm = SVC(probability=True, class_weight='balanced')
+# print('TASK1: predicting probabilities...')
+# # we let the linear svm fit and predict each test individually
+# for label in classification_labels:
+#     svm.fit(df_train_features.to_numpy(), df_train_labels[label].to_numpy())
+#     # compute probabilities
+#     pred = svm.predict(df_test_features.to_numpy())
+#     prob = svm.predict_proba(df_test_features.to_numpy())
+#     df_test_labels[label] = [p[1] for p in prob]
+#     print('...predicted', label)
+# print('...done')
 
 # ======================
 # Task 2: predict sepsis
 # ======================
-svm = SVC(probability=True, class_weight='balanced')
-print('TASK2: predicting probabilities...')
-svm.fit(df_train_features.to_numpy(), df_train_labels[sepsis_label].to_numpy())
-pred = svm.predict(df_test_features.to_numpy())
-prob = svm.predict_proba(df_test_features.to_numpy())
-df_test_labels[sepsis_label] = [p[1] for p in prob]
-print('...done')
+# svm = SVC(probability=True, class_weight='balanced')
+# print('TASK2: predicting probabilities...')
+# svm.fit(df_train_features.to_numpy(), df_train_labels[sepsis_label].to_numpy())
+# pred = svm.predict(df_test_features.to_numpy())
+# prob = svm.predict_proba(df_test_features.to_numpy())
+# df_test_labels[sepsis_label] = [p[1] for p in prob]
+# print('...done')
 
 # ===========================
 # Task 3: predict vital signs
 # ===========================
-svm = SVR(kernel='rbf')
-print('TASK3: predicting regression values')
-# we let the svm fit and predict each vital sign individually
-for label in regression_labels:
-    svm.fit(df_train_features.to_numpy(), df_train_labels[label].to_numpy())
-    # compute distance to hyperplane
-    pred = svm.predict(df_test_features.to_numpy())
-    df_test_labels[label] = pred
-    print('...predicted', label)
-print('...done')
+# svm = SVR(kernel='rbf')
+# print('TASK3: predicting regression values')
+# # we let the svm fit and predict each vital sign individually
+# for label in regression_labels:
+#     svm.fit(df_train_features.to_numpy(), df_train_labels[label].to_numpy())
+#     # compute distance to hyperplane
+#     pred = svm.predict(df_test_features.to_numpy())
+#     df_test_labels[label] = pred
+#     print('...predicted', label)
+# print('...done')
 
 # suppose df is a pandas dataframe containing the result
-df_test_labels.to_csv('prediction.zip', index=True, float_format='%.3f', compression='zip')
-df_test_labels.to_csv('prediction.csv', index=True, float_format='%.3f')
+# df_test_labels.to_csv('prediction.zip', index=True, float_format='%.3f', compression='zip')
+# df_test_labels.to_csv('prediction.csv', index=True, float_format='%.3f')
 
 # ====================
 # evaluate performance
