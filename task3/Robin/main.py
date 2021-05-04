ed79979ad605c584a99f720fb9a7af3bdb61d946
Robin Schmid <schmirob@student.ethz.ch>
2021-05-04 11:09:53 +0200

    Simple NN for binary classification

--- task3/Robin/main.py
+++ task3/Robin/main.py
@@ -5,13 +5,24 @@ Robin Schmid, Pascal Mueller, Marvin Harms
 May, 2021
 """
 
+import time
+import seaborn as sns
+
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import f1_score
 from sklearn.preprocessing import StandardScaler
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+## Preprocessing
 df_train = pd.read_csv("../handout/train.csv")
 df_test = pd.read_csv("../handout/test.csv")
 
@@ -30,14 +41,154 @@ enc.fit(X_train)
 X_train_encoded = enc.transform(X_train).toarray()
 X_test_encoded = enc.transform(X_test).toarray()
 
+# Data is very imbalanced
+# sns.countplot(x = 'Active', data=df_train)
+# plt.show()
+
 # Scale data
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train_encoded)
-X_test_scaled = scaler.transform(X_train_encoded)
+X_test_scaled = scaler.transform(X_test_encoded)
+
+# Model parameters
+EPOCHS = 50
+BATCH_SIZE = 64
+LEARNING_RATE = 0.01
+
+# Data loaders
+# Train data
+class trainData(Dataset):
+
+    def __init__(self, X_data, y_data):
+        self.X_data = X_data
+        self.y_data = y_data
+
+    def __getitem__(self, index):
+        return self.X_data[index], self.y_data[index]
+
+    def __len__(self):
+        return len(self.X_data)
 
 
+train_data = trainData(torch.FloatTensor(X_train_scaled),
+                       torch.FloatTensor(y_train))
 
+# Test data
+class testData(Dataset):
 
+    def __init__(self, X_data):
+        self.X_data = X_data
 
+    def __getitem__(self, index):
+        return self.X_data[index]
 
+    def __len__(self):
+        return len(self.X_data)
 
+test_data = testData(torch.FloatTensor(X_test_scaled))
+
+# Initialize data loaders
+train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(dataset=test_data, batch_size=1)
+
+# Define architecture
+class binaryClassification(nn.Module):
+    def __init__(self):
+        super(binaryClassification, self).__init__()
+        self.layer_1 = nn.Linear(len(X_test_scaled[0]), 64)
+        self.layer_2 = nn.Linear(64, 64)
+        self.layer_out = nn.Linear(64, 1)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.1)
+        self.batchnorm1 = nn.BatchNorm1d(64)
+        self.batchnorm2 = nn.BatchNorm1d(64)
+
+    def forward(self, inputs):
+        x = self.relu(self.layer_1(inputs))
+        x = self.batchnorm1(x)
+        x = self.relu(self.layer_2(x))
+        x = self.batchnorm2(x)
+        x = self.dropout(x)
+        x = self.layer_out(x)
+
+        return x
+
+# Optimization parameters
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Use cpu here
+model = binaryClassification()
+model.to(device)
+
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+# Define accuracy function
+def binary_acc(y_pred, y_test):
+    y_pred_tag = torch.round(torch.sigmoid(y_pred))
+
+    correct_results_sum = (y_pred_tag == y_test).sum().float()
+    acc = correct_results_sum / y_test.shape[0]
+    acc = torch.round(acc * 100)
+
+    return acc
+
+def f1_loss(y_true: torch.Tensor, y_pred: torch.Tensor, is_training=False) -> torch.Tensor:
+
+    assert y_true.ndim == 1
+    assert y_pred.ndim == 1 or y_pred.ndim == 2
+
+    if y_pred.ndim == 2:
+        y_pred = y_pred.argmax(dim=1)
+
+    tp = (y_true * y_pred).sum().to(torch.float32)
+    tn = ((1 - y_true) * (1 - y_pred)).sum().to(torch.float32)
+    fp = ((1 - y_true) * y_pred).sum().to(torch.float32)
+    fn = (y_true * (1 - y_pred)).sum().to(torch.float32)
+
+    epsilon = 1e-7
+
+    precision = tp / (tp + fp + epsilon)
+    recall = tp / (tp + fn + epsilon)
+
+    f1 = 2 * (precision * recall) / (precision + recall + epsilon)
+    f1.requires_grad = is_training
+    return f1
+
+# Train loop
+model.train()
+for e in range(1, EPOCHS + 1):
+    epoch_loss = 0
+    epoch_acc = 0
+    for X_batch, y_batch in train_loader:
+        X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+        optimizer.zero_grad()
+
+        y_pred = model(X_batch)
+
+        loss = criterion(y_pred, y_batch.unsqueeze(1))
+        acc = binary_acc(y_pred, y_batch.unsqueeze(1))
+        # acc = f1_loss(y_batch.unsqueeze(1),y_pred)
+
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    print(f'Epoch {e + 0:03}: | Loss: {epoch_loss / len(train_loader):.5f} | Acc: {epoch_acc / len(train_loader):.3f}')
+
+# Evaluate model
+y_pred_list = []
+model.eval()
+with torch.no_grad():
+    for X_batch in test_loader:
+        X_batch = X_batch.to(device)
+        y_test_pred = model(X_batch)
+        y_test_pred = torch.sigmoid(y_test_pred)
+        y_pred_tag = torch.round(y_test_pred)
+        y_pred_list.append(y_pred_tag.cpu().numpy())
+
+y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
+
+np.savetxt("predictions.csv", y_pred_list, fmt="%i")
