4bfe74901b5da91254da5a90d0f98b2d47db6615
Robin Schmid <schmirob@student.ethz.ch>
2021-05-12 22:09:00 +0200

    Updated code Marvin, new highest score

--- task3/Robin/main.py
+++ task3/Robin/main.py
@@ -4,75 +4,50 @@ Team Naiveoutliers
 Robin Schmid, Pascal Mueller, Marvin Harms
 May, 2021
 """
-
-"""
-TODO:
--f1 score for weighted random sampler seems wrong (bigger than 1)?
--implement f1 score of sklearn, not own built in fct
--f1 score seems very low now, not handling the imbalanced dataset well, maybe there are better ways than just using a scaler to the data?
--try weighting in binary classification loss to deal with imbalanced classes - implemented, does not give better results, error?
--try cross entropy loss instead of binary classification loss
--power transformer instead of standard scaler for rescaling of the data?
--later change model architecture, i.e. change layer sizes
-"""
-
 import time
-import seaborn as sns
-
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import f1_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import PowerTransformer
-
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
-from torch.utils.data.sampler import WeightedRandomSampler
+import sklearn
+from sklearn.metrics import f1_score
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder,StandardScaler, PowerTransformer
 
-### Preprocessing
-df_train = pd.read_csv("../handout/train.csv")
-df_test = pd.read_csv("../handout/test.csv")
-
-# Split strings of amino acids, maintaining the sites
-X_train = df_train['Sequence'].values
-X_train = [list(X_train[i]) for i in range(len(X_train))]
-X_test = df_test['Sequence'].values
-X_test = [list(X_test[i]) for i in range(len(X_test))]
-
-# Get label of mutation
-y_train = df_train['Active'].values
-
-# Encode data with one hot encoding, preserve the order of the mutation
-enc = OneHotEncoder(handle_unknown='ignore')
-enc.fit(X_train)
-X_train_encoded = enc.transform(X_train).toarray()
-X_test_encoded = enc.transform(X_test).toarray()
-
-# Data is very imbalanced
-# sns.countplot(x = 'Active', data=df_train)
-# plt.show()
-
-# Scale data
-scaler = StandardScaler()
-# scaler = PowerTransformer()
-X_train_scaled = scaler.fit_transform(X_train_encoded)
-X_test_scaled = scaler.transform(X_test_encoded)
-
-### Network architecture
-# Model parameters
-EPOCHS = 50
-BATCH_SIZE = 64
+#######################
+EPOCHS = 100
+BATCH_SIZE = 256
 LEARNING_RATE = 0.002
+#######################
 
-# Data loaders
-# Train data
+# set random seed
+np.random.seed(42)
+torch.manual_seed(42)
+
+class AverageMeter():
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+# Train data loader
 class trainData(Dataset):
-
+    '''data loader for training data'''
     def __init__(self, X_data, y_data):
         self.X_data = X_data
         self.y_data = y_data
@@ -83,13 +58,9 @@ class trainData(Dataset):
     def __len__(self):
         return len(self.X_data)
 
-
-train_data = trainData(torch.FloatTensor(X_train_scaled),
-                       torch.FloatTensor(y_train))
-
-# Test data
+# Test data loader
 class testData(Dataset):
-
+    '''data loader for test data'''
     def __init__(self, X_data):
         self.X_data = X_data
 
@@ -99,123 +70,179 @@ class testData(Dataset):
     def __len__(self):
         return len(self.X_data)
 
-test_data = testData(torch.FloatTensor(X_test_scaled))
-
-# Try different sampling for imbalanced dataset
-
-
-# Initialize data loaders
-# Naive data loader
-train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
-
-# TODO: check this implementation, does not give better results, f1 seems off
-# Weighted data loader
-# counts = np.bincount(y_train)
-# labels_weights = 1. / counts
-# weights = labels_weights[y_train]
-# train_sampler = WeightedRandomSampler(weights, len(weights))
-# train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE,sampler=train_sampler, shuffle=False)
-
-test_loader = DataLoader(dataset=test_data, batch_size=1)
-
 # Architecture
 class binaryClassification(nn.Module):
+    '''class to define NN architecture'''
     def __init__(self):
         super(binaryClassification, self).__init__()
-        self.layer_1 = nn.Linear(len(X_test_scaled[0]), 64)
-        self.layer_2 = nn.Linear(64, 64)
-        self.layer_out = nn.Linear(64, 1)
+        self.classifier1 = nn.Sequential(
+            nn.Linear(len(X_test_enc[0]), 128),
+            nn.ReLU(),
+            nn.BatchNorm1d(128),
 
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.1)
-        self.batchnorm1 = nn.BatchNorm1d(64)
-        self.batchnorm2 = nn.BatchNorm1d(64)
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.BatchNorm1d(128),
+
+            nn.Linear(128,1)
+        )
+        self.classifier2 = nn.Sequential(
+            nn.Linear(len(X_test_enc[0]), 128),
+            nn.ReLU(),
+            nn.BatchNorm1d(128),
+
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.BatchNorm1d(128),
+
+            nn.Linear(128, 1)
+        )
+
 
     def forward(self, inputs):
-        x = self.relu(self.layer_1(inputs))
-        x = self.batchnorm1(x)
-        x = self.relu(self.layer_2(x))
-        x = self.batchnorm2(x)
-        x = self.dropout(x)
-        x = self.layer_out(x)
-
-        return x
-
-# Optimization parameters
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# Use cpu here
-model = binaryClassification()
-model.to(device)
-
-criterion = nn.BCEWithLogitsLoss()
-optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-
-# Define accuracy function
-def binary_acc(y_pred, y_test):
-    y_pred_tag = torch.round(torch.sigmoid(y_pred))
-
-    correct_results_sum = (y_pred_tag == y_test).sum().float()
-    acc = correct_results_sum / y_test.shape[0]
-    acc = torch.round(acc * 100)
-
-    return acc
+        prediction = self.classifier2(inputs)
+        return prediction
 
 def f1_acc(y_pred, y_true):
+
     tp = (y_true * y_pred).sum().float()
     tn = ((1 - y_true) * (1 - y_pred)).sum().float()
     fp = ((1 - y_true) * y_pred).sum().float()
     fn = (y_true * (1 - y_pred)).sum().float()
-
     epsilon = 1e-7
-
     precision = tp / (tp + fp + epsilon)
     recall = tp / (tp + fn + epsilon)
-
     f1 = 2 * (precision * recall) / (precision + recall + epsilon)
     return f1
 
-# Train loop
+# ============
+# load dataset
+# ============
+df_train = pd.read_csv("../handout/train.csv")
+df_test = pd.read_csv("../handout/test.csv")
+# get training data
+X_train = df_train['Sequence'].values
+X_test = df_test['Sequence'].values
+# now, we split the strings into lists:
+X_train = [list(X_train[i]) for i in range(len(X_train))]
+X_test = [list(X_test[i]) for i in range(len(X_test))]
+
+y_train = df_train['Active'].values
+
+# ===================================================
+# perform feature encoding of the dataset
+# we choose a 4dim feature vector and ordinal encoder
+# ===================================================
+enc = OneHotEncoder(handle_unknown='ignore')
+enc.fit(X_train)
+X_train_enc = enc.transform(X_train).toarray()
+X_test_enc = enc.transform(X_test).toarray()
+print(enc.categories_)
+
+#=================
+# Standardize data
+# ================
+#scaler = StandardScaler()
+scaler = PowerTransformer()
+scaler.fit(X_train_enc)
+X_train_enc = scaler.transform(X_train_enc)
+X_test_enc = scaler.transform(X_test_enc)
+
+# =========
+# create NN
+# =========
+model = binaryClassification()
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+# consider reweighting the classes due to heavily imbalanced dataset
+pos_frac = np.sum(y_train)/len(y_train)
+pos_weight = (1-pos_frac)/pos_frac
+criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(pos_weight))
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+# =================
+# run training loop
+# =================
 model.train()
-for e in range(1, EPOCHS + 1):
-    epoch_loss = 0
-    epoch_acc = 0
-    for X_batch, y_batch in train_loader:
-        X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+train_results = {}
+test_results = {}
+# iterate over all epochs
+for epoch in range(1, EPOCHS+1):
+    time_ = AverageMeter()
+    loss_ = AverageMeter()
+    acc_ = AverageMeter()
+    # ===================
+    # Define data loaders
+    # ===================
+    train_data = trainData(torch.FloatTensor(X_train_enc),
+                        torch.FloatTensor(y_train))
+    test_data = testData(torch.FloatTensor(X_test_enc))
+    train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = DataLoader(dataset=test_data, batch_size=BATCH_SIZE, shuffle=False)
+    # iterate over training minibatches
+    for i, data, in enumerate(train_loader, 1):
+        # Accounting
+        end = time.time()
+        features,labels = data
+        features = features.to(device)
+        labels = labels.to(device)
+        bs = features.size(0)
+
+        # zero the parameter gradients
         optimizer.zero_grad()
-
-        y_pred = model(X_batch)
-
-        loss = criterion(y_pred, y_batch.unsqueeze(1))
-
-        # Different accuracies
-        # acc = binary_acc(y_pred, y_batch.unsqueeze(1))
-        acc = f1_acc(y_batch.unsqueeze(1),torch.round(torch.sigmoid(y_pred)))
-
-        # TODO: Using f1 score build in function of sklearn gives an error, y_batch is binary and needs to be a float too, not resolved yet
-        # vector = np.vectorize(np.float)
-        # y_batch = vector(y_batch.unsqueeze(1).detach().numpy())
-        # print(y_batch.unsqueeze(1).detach().numpy().astype(float))
-        # acc = f1_score(y_batch.unsqueeze(1).to(torch.float32).detach().numpy(),y_pred.detach().numpy())
-
+        # forward propagation
+        prediction = model(features)
+        # compute the loss
+        loss = criterion(prediction, labels.unsqueeze(1))
+        # Backward propagation
         loss.backward()
+        # Optimization step.
         optimizer.step()
 
-        epoch_loss += loss.item()
-        epoch_acc += acc.item()
+        # Accounting
+        acc = f1_acc(torch.round(torch.sigmoid(prediction)), labels.unsqueeze(1))
+        loss_.update(loss.mean().item(), bs)
+        acc_.update(acc.item(), bs)
+        time_.update(time.time() - end)
 
-    print(f'Epoch {e + 0:03}: | Loss: {epoch_loss / len(train_loader):.5f} | Acc: {epoch_acc / len(train_loader):.3f}')
+    print(f'Epoch {epoch}. [Train] \t Time {time_.sum:.2f} Loss \
+            {loss_.avg:.2f} \t Accuracy {acc_.avg:.2f}')
+    train_results[epoch] = (loss_.avg, acc_.avg, time_.avg)
 
-# Evaluate model
+# plot training process
+# training = list()
+# for key, values in train_results.items():
+#     training.append([key, values[0], values[1], values[2]])
+# training = list(map(list, zip(*training)))
+#
+# fig, axs = plt.subplots(2)
+# fig.suptitle('Loss and accuracy per epoch')
+# axs[0].plot(training[0], training[1], 'b')
+# axs[0].set_ylabel('loss')
+# axs[0].grid()
+# axs[1].plot(training[0], training[2], 'b')
+# axs[1].set_ylabel('accuracy')
+# axs[1].set_xlabel('epoch')
+# axs[1].grid()
+# plt.show()
+
+
+# ===================
+# run prediction loop
+# ===================
 y_pred_list = []
 model.eval()
-with torch.no_grad():
-    for X_batch in test_loader:
-        X_batch = X_batch.to(device)
-        y_test_pred = model(X_batch)
-        y_test_pred = torch.sigmoid(y_test_pred)
-        y_pred_tag = torch.round(y_test_pred)
-        y_pred_list.append(y_pred_tag.cpu().numpy())
+for data in test_loader:
+    with torch.no_grad():
+        features = data.to(device)
+        prediction = model(features)
+        prediction = torch.sigmoid(prediction)
+        y_pred = torch.round(prediction)
+        y_pred_list.append(y_pred.cpu().numpy())
 
-y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
-
+# =======================
+# save predictions to csv
+# =======================
+y_pred_list = [batch.squeeze().tolist() for batch in y_pred_list]
+y_pred_list = np.concatenate(y_pred_list).ravel().tolist()
 np.savetxt("predictions.csv", y_pred_list, fmt="%i")
