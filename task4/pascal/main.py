5d20a1ea0a2616b8f93f32c5f393fcb8be2057d4
PMU <pascal.mueller@thurweb.ch>
2021-05-17 18:12:10 +0200

    init pascal

---
+++ task4/pascal/main.py
@@ -0,0 +1,13 @@
+"""
+Introduction to Machine Learning
+Task 4: Open-ended
+Team Naiveoutliers
+Robin Schmid, Marvin Harms, Pascal Müller
+May, 2021
+"""
+
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+
