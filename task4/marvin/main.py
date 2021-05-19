1546e12dba924d261de0cf108e26a2cc45bb5f56
harmsm <harmsm@student.ethz.ch>
2021-05-19 17:01:14 +0200

    constructed features and labels

--- task4/marvin/main.py
+++ task4/marvin/main.py
@@ -15,10 +15,89 @@ from keras.preprocessing.image import img_to_array
 from keras.applications.vgg16 import preprocess_input
 from keras.applications.resnet50 import decode_predictions
 from keras.applications.resnet50 import ResNet50
-from keras.layers import Flatten
-from keras.layers import Dense
-from keras.models import Model
+# pytorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import sklearn
 
+
+#######################
+USE_PREPROCESSED = True
+LEARNING_RATE = 0.0002
+BATCHSIZE = 64
+EPOCHS = 10
+#######################
+
+# set random seed
+np.random.seed(42)
+
+
+class AverageMeter():
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+# Train data loader
+class trainData(Dataset):
+    '''data loader for training data'''
+    def __init__(self, X_data, y_data):
+        self.X_data = X_data
+        self.y_data = y_data
+
+    def __getitem__(self, index):
+        return self.X_data[index], self.y_data[index]
+
+    def __len__(self):
+        return len(self.X_data)
+
+# Test data loader
+class testData(Dataset):
+    '''data loader for test data'''
+    def __init__(self, X_data):
+        self.X_data = X_data
+
+    def __getitem__(self, index):
+        return self.X_data[index]
+
+    def __len__(self):
+        return len(self.X_data)
+
+# Architecture
+class binaryClassification(nn.Module):
+    '''class to define NN architecture'''
+    def __init__(self):
+        super(binaryClassification, self).__init__()
+        self.classifier = nn.Sequential(
+            nn.Linear(len(X_train[0]), 256),
+            nn.ReLU(),
+            #nn.Linear(256, 256),
+            #nn.ReLU(),
+            nn.Linear(256,64),
+            nn.ReLU(),
+            nn.Dropout(p=0.1),
+            nn.Linear(64,1),
+            nn.Sigmoid()
+        )
+
+
+    def forward(self, inputs):
+        prediction = self.classifier2(inputs)
+        return prediction
 
 
 def get_image(filename):
@@ -27,56 +106,203 @@ def get_image(filename):
     img = img_to_array(img)
     return img
 
+#=======================================================================================
 # IDEA: PREDICT CLASS LABELS FOR EACH IMAGE INDEPENDENTLY.
 # THEN, WE SAVE THE 1000-DIM OUTPUT VECTOR OF EACH IMAGE AS A NEW FEATURE VECTOR.
-# WE THEN TRAIN A NEURAL NETWORK WITH 2x1000 INPUTS AND ONE SINGLE OUTPUT 
+# WE THEN TRAIN A NEURAL NETWORK WITH 3x1000 INPUTS AND ONE SINGLE OUTPUT 
 # TO PREDICT A TASTE SIMILARITY BETWEEN THE TWO INPUTS IN RANGE [0,1].
+#=======================================================================================
 
-
-# =========
-# load data
-# =========
 handout_path = "/home/marvin/Downloads/IML_handout_task4/"
-# load an image from file
-df_train = pd.read_csv(handout_path+'/train_triplets.txt')
-df_test = pd.read_csv(handout_path+'/test_triplets.txt')
 
 # ===========
 # setup model
 # ===========
-# we use the pretrained model and change the input and output layers
-# the input layer includes 3 stacked images with 3 layers each
-# the output is a scalar in range [0,1]
+# we use the pretrained model
 model = tf.keras.Sequential()
 model = ResNet50(include_top=True)
 model.summary()
 
+if not USE_PREPROCESSED:
+    # =====================================
+    # predict image classes (preprocessing)
+    # =====================================
+    header = []
+    for i in range(1000):
+        header.append('class'+str(i+1))
 
-# predict image classes
-header = []
-for i in range(1000):
-    header.append(str(i+1))
+    df = pd.DataFrame(columns=header)
+    imagenames = sorted(os.listdir(handout_path+'food/'))
+    for filename in sorted(os.listdir(handout_path+'food/')):
+        if filename.endswith('.jpg'):
+            print('\n'+filename)
+            image = get_image(filename)
+            # reshape data for the model
+            image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+            # prepare the image for the model
+            image = preprocess_input(image)
+            # predict the probability across all output classes
+            yhat = model.predict(image)
+            # retrieve the most likely result, e.g. highest probability
+            label = decode_predictions(yhat)
+            # retrieve the most likely result, e.g. highest probability
+            label = label[0][0]
     
-df = pd.DataFrame(columns=header)
-imagenames = sorted(os.listdir(handout_path+'food/'))
+            print('%s (%.2f%%)' % (label[1], label[2]*100))
+            # append the probabilities to dataframe
+            df.loc[filename[0:-4]] = yhat[0][:]
 
-for filename in sorted(os.listdir(handout_path+'food/')):
-    if filename.endswith('.jpg'):
-        print('\n'+filename)
-        image = get_image(filename)
-        # reshape data for the model
-        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-        # prepare the image for the model
-        image = preprocess_input(image)
-        # predict the probability across all output classes
-        yhat = model.predict(image)
-        # retrieve the most likely result, e.g. highest probability
-        label = decode_predictions(yhat)
-        # retrieve the most likely result, e.g. highest probability
-        label = label[0][0]
- 
-        print('%s (%.2f%%)' % (label[1], label[2]*100))
-        # append the probabilities to dataframe
-        df.loc[filename] = yhat[0][:]
+    df.to_csv(handout_path+'class_probabilities.csv')
 
-df.to_csv(handout_path+'class_probabilities.csv')
+else:
+    # ========================
+    # load preprocessed images
+    # ========================
+    df = pd.read_csv(handout_path+'class_probabilities.csv')
+
+# =====================================
+# split the data in train and test sets
+# =====================================
+df_train = pd.read_csv(handout_path+'/train_triplets.txt')
+df_test = pd.read_csv(handout_path+'/test_triplets.txt')
+
+# =======================
+# construct training data
+# =======================
+X_train = []
+y_train = []
+counter = 0
+iterations = len(df_train.values)
+for triplet in df_train.values:
+    triplet = [int(img) for img in triplet[0].split(' ')]
+    # for each triplet, we can construct two possible outputs by switching image B and C
+    row1 = [df.loc[triplet[0]].values,df.loc[triplet[1]].values,df.loc[triplet[2]].values]
+    row2 = [df.loc[triplet[0]].values,df.loc[triplet[2]].values,df.loc[triplet[1]].values]
+    if counter==0:
+        X_train = np.concatenate((row1,row2),axis=0)
+        y_train = np.concatenate(([1],[0]),axis=0)
+
+    else:
+        X_train = np.concatenate((X_train,row1,row2),axis=0)
+        y_train = np.concatenate((y_train,[1],[0]),axis=0)
+
+    counter += 1
+    print(counter/iterations)
+
+# ===================
+# construct test data
+# ===================
+X_test = []
+y_test = []
+counter = 0
+iterations = len(df_test.values)
+for triplet in df_test.values:
+    triplet = [int(img) for img in triplet[0].split(' ')]
+    row = [df.loc[triplet[0]].values,df.loc[triplet[1]].values,df.loc[triplet[2]].values]
+    if counter==0:
+        X_test = row
+
+    else:
+        X_test = np.concatenate((X_train,row1,row2),axis=0)
+
+
+    counter += 1
+    print(counter/iterations)
+
+# ====================
+# create NN classifier
+# ====================
+model = binaryClassification()
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE)
+
+# =================
+# run training loop
+# =================
+model.train()
+train_results = {}
+test_results = {}
+# iterate over all epochs
+for epoch in range(1, EPOCHS+1):
+    time_ = AverageMeter()
+    loss_ = AverageMeter()
+    acc_ = AverageMeter()
+    # ===================
+    # Define data loaders
+    # ===================
+    train_data = trainData(torch.FloatTensor(X_train),
+                        torch.FloatTensor(y_train))
+    test_data = testData(torch.FloatTensor(X_test))
+    train_loader = DataLoader(dataset=train_data, batch_size=BATCHSIZE, shuffle=True)
+    test_loader = DataLoader(dataset=test_data, batch_size=BATCHSIZE, shuffle=False)
+    # iterate over training minibatches
+    for i, data, in enumerate(train_loader, 1):
+        # Accounting
+        end = time.time()
+        features,labels = data
+        features = features.to(device)
+        labels = labels.to(device)
+        bs = features.size(0)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        # forward propagation
+        prediction = model(features) 
+        # compute the loss
+        loss = criterion(prediction, labels.unsqueeze(1))
+        # Backward propagation
+        loss.backward()
+        # Optimization step. 
+        optimizer.step()
+
+        # Accounting
+        acc = f1_acc(torch.round(torch.sigmoid(prediction)), labels.unsqueeze(1))
+        loss_.update(loss.mean().item(), bs)
+        acc_.update(acc.item(), bs)
+        time_.update(time.time() - end)
+
+    print(f'Epoch {epoch}. [Train] \t Time {time_.sum:.2f} Loss \
+            {loss_.avg:.2f} \t Accuracy {acc_.avg:.2f}')
+    train_results[epoch] = (loss_.avg, acc_.avg, time_.avg)
+
+# plot training process
+training = list()
+for key, values in train_results.items():
+    training.append([key, values[0], values[1], values[2]])
+training = list(map(list, zip(*training)))
+
+fig, axs = plt.subplots(2)
+fig.suptitle('Loss and accuracy per epoch')
+axs[0].plot(training[0], training[1], 'b')
+axs[0].set_ylabel('loss')
+axs[0].grid()
+axs[1].plot(training[0], training[2], 'b')
+axs[1].set_ylabel('accuracy')
+axs[1].set_xlabel('epoch')
+axs[1].grid()
+plt.show()
+
+
+# ===================
+# run prediction loop
+# ===================
+y_pred_list = []
+model.eval()
+for data in test_loader:
+    with torch.no_grad():
+        features = data.to(device)
+        prediction = model(features)
+        prediction = torch.sigmoid(prediction)
+        y_pred = torch.round(prediction)
+        y_pred_list.append(y_pred.cpu().numpy())               
+
+# =======================
+# save predictions to csv
+# =======================
+y_pred_list = [batch.squeeze().tolist() for batch in y_pred_list]   
+y_pred_list = np.concatenate(y_pred_list).ravel().tolist()           
+np.savetxt("predictions.csv", y_pred_list, fmt="%i")
+
+    
