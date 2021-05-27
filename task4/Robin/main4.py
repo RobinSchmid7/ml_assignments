102561dbdd139f05a7aea4285591ba50cb612ccd
Robin Schmid <schmirob@student.ethz.ch>
2021-05-27 21:32:47 +0200

    Added todos version 4

--- task4/Robin/main4.py
+++ task4/Robin/main4.py
@@ -9,24 +9,31 @@ May, 2021
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Lambda
-from tensorflow.keras import optimizers, Sequential, Input
+from tensorflow.keras import optimizers, Sequential, Input, Model
 from tensorflow.keras.applications import Xception
 from sklearn.model_selection import train_test_split
 
-WIDTH = 96
-HEIGHT = 96
+WIDTH = 100
+HEIGHT = 100
 CHANNELS = 3
 
 VALIDATION_SIZE = 0.15
-NUM_EMBEDDINGS = 300
+NUM_EMBEDDINGS = 200
 LEARNING_RATE = 0.001
-EPOCHS = 2
+EPOCHS = 10
 
 TRAIN_BATCH_SIZE = 32
-TEST_BATCH_SIZE = 64
+TEST_BATCH_SIZE = 256
 BUFFER_SIZE = 1024
 
 HANDOUT_PATH = '../handout/'
+
+# TODO:
+# - change fct and code structure a lot
+# - run 1 training loop once and upload results
+# - play around with tuning parameters
+# - try a different penalty for the difference in triplet loss
+# - use ideas from hartout, save model as json file etc.
 
 # Preprocessing
 def create_split_data():
@@ -54,11 +61,11 @@ def load_triplets(triplet):
     anchor = process_image(HANDOUT_PATH + '/food/' + idx[0] + '.jpg')
     positive = process_image(HANDOUT_PATH + '/food/' + idx[1] + '.jpg')
     negative = process_image(HANDOUT_PATH + '/food/' + idx[2] + '.jpg')
-    return tf.stack([anchor, positive, negative], axis=0)
+    return tf.stack([anchor, positive, negative], axis=0), 1
 
 def process_image(filename):
     img = tf.io.read_file(filename)
-    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.decode_jpeg(img, channels=CHANNELS)
     img = tf.cast(img, tf.float32)
     img = tf.image.resize(img, (HEIGHT, WIDTH))
     return img
@@ -83,7 +90,7 @@ def create_model(num_embeddings):
     embeddings = tf.stack([anchor_embedding, positive_embedding, negative_embedding], axis=-1)
 
     # Siamese model
-    siamese_model = tf.keras.Model(inputs=basis_inputs, outputs=embeddings)
+    siamese_model = Model(inputs=basis_inputs, outputs=embeddings)
     # siamese_model.summary()
 
     return siamese_model
@@ -92,7 +99,7 @@ def create_prediction_model(model):
     dist_positive = tf.reduce_sum(tf.square(model.output[..., 0] - model.output[..., 1]), axis=1)
     dist_negative = tf.reduce_sum(tf.square(model.output[..., 0] - model.output[..., 2]), axis=1)
     predictions = tf.cast(tf.greater_equal(dist_negative, dist_positive), tf.int8)
-    return tf.keras.Model(inputs=model.inputs, outputs=predictions)
+    return Model(inputs=model.inputs, outputs=predictions)
 
 def triplet_loss(_,embeddings):
     dist_positive = tf.reduce_sum(tf.square(embeddings[..., 0] - embeddings[..., 1]), axis=1)
@@ -103,7 +110,6 @@ def accuracy(_,embeddings):
     dist_positive = tf.reduce_sum(tf.square(embeddings[..., 0] - embeddings[..., 1]), axis=1)
     dist_negative = tf.reduce_sum(tf.square(embeddings[..., 0] - embeddings[..., 2]), axis=1)
     return tf.reduce_mean(tf.cast(tf.greater_equal(dist_negative, dist_positive), tf.float32))
-
 
 if __name__ == '__main__':
     # Load data
@@ -120,7 +126,6 @@ if __name__ == '__main__':
     # Create model
     model = create_model(NUM_EMBEDDINGS)
     model.compile(optimizer=optimizers.Adam(learning_rate=LEARNING_RATE), loss=triplet_loss, metrics=[accuracy])
-    # model.summary()
 
     # Load batches
     train_data = train_data.shuffle(BUFFER_SIZE, reshuffle_each_iteration=True).repeat().batch(TRAIN_BATCH_SIZE)
@@ -130,11 +135,11 @@ if __name__ == '__main__':
     model.fit(train_data, steps_per_epoch=int(np.ceil(num_train_data / TRAIN_BATCH_SIZE)),
               epochs=EPOCHS,validation_data=val_data,validation_steps=10)
 
+    # Predict labels
     test_data = load_dataset(HANDOUT_PATH + 'test_triplets.txt')
     test_data = test_data.batch(TEST_BATCH_SIZE).prefetch(2)
     prediction_model = create_prediction_model(model)
-
     predictions = prediction_model.predict(test_data,steps=int(np.ceil(num_test_data / TEST_BATCH_SIZE)),verbose=1)
-    np.savetxt('predictions3.txt', predictions, fmt='%i')
 
+    np.savetxt('predictions.txt', predictions, fmt='%i')
     print("Finished!")
