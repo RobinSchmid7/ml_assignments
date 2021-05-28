da29cc123a7787958180c8a843272c256fea15a1
Robin Schmid <schmirob@student.ethz.ch>
2021-05-28 20:38:17 +0200

    Cleanup and augmentation

--- task4/Robin/main4.py
+++ task4/Robin/main4.py
@@ -10,32 +10,28 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Lambda
 from tensorflow.keras import optimizers, Sequential, Input, Model
-from tensorflow.keras.applications import Xception
+from tensorflow.keras.applications import Xception, ResNet50, MobileNetV2
 from sklearn.model_selection import train_test_split
 
-WIDTH = 100
-HEIGHT = 100
+USE_TRAINED = False
+
+WIDTH = 96
+HEIGHT = 96
 CHANNELS = 3
 
-VALIDATION_SIZE = 0.15
-NUM_EMBEDDINGS = 200
+VALIDATION_SIZE = 0.2
+VALIDATION_STEPS = 10
+NUM_EMBEDDINGS = 128
 LEARNING_RATE = 0.001
-EPOCHS = 10
+EPOCHS = 5
 
-TRAIN_BATCH_SIZE = 32
+TRAIN_BATCH_SIZE = 64
 TEST_BATCH_SIZE = 256
 BUFFER_SIZE = 1024
 
 HANDOUT_PATH = '../handout/'
 
-# TODO:
-# - change fct and code structure a lot
-# - run 1 training loop once and upload results
-# - play around with tuning parameters
-# - try a different penalty for the difference in triplet loss
-# - use ideas from hartout, save model as json file etc.
-
-# Preprocessing
+# Preprocessing, split training triplets into training and validation set
 def create_split_data():
     with open(HANDOUT_PATH + 'train_triplets.txt', 'r') as file:
         triplets = [l for l in file.readlines()]
@@ -49,31 +45,35 @@ def create_split_data():
         for e in val_data:
             file.write(e)
 
-def load_dataset(filename):
+def load_dataset(filename, augmenting):
     data = tf.data.TextLineDataset(filename)
-    data = data.map(lambda triplet: load_triplets(triplet),
+    data = data.map(lambda triplet: load_triplets(triplet, augmenting),
                           num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    # Returns a tf tensor containing all triplets of the file, each element is a image tensor
     return data
 
-def load_triplets(triplet):
+def load_triplets(triplet, augmenting):
     idx = tf.strings.split(triplet)
-    anchor = process_image(HANDOUT_PATH + '/food/' + idx[0] + '.jpg')
-    positive = process_image(HANDOUT_PATH + '/food/' + idx[1] + '.jpg')
-    negative = process_image(HANDOUT_PATH + '/food/' + idx[2] + '.jpg')
+    anchor = process_image(HANDOUT_PATH + '/food/' + idx[0] + '.jpg', augmenting)
+    positive = process_image(HANDOUT_PATH + '/food/' + idx[1] + '.jpg', augmenting)
+    negative = process_image(HANDOUT_PATH + '/food/' + idx[2] + '.jpg', augmenting)
     return tf.stack([anchor, positive, negative], axis=0), 1
 
-def process_image(filename):
+def process_image(filename, augmenting):
     img = tf.io.read_file(filename)
     img = tf.image.decode_jpeg(img, channels=CHANNELS)
-    img = tf.cast(img, tf.float32)
+    # Resize and scale images
+    img = tf.cast(img, tf.float32) / 127.5 - 1
     img = tf.image.resize(img, (HEIGHT, WIDTH))
+    # Randomly flip images for augmentation
+    if augmenting:
+        img = tf.image.random_flip_left_right(img)
+        img = tf.image.random_flip_up_down(img)
     return img
 
 def create_model(num_embeddings):
     # Pretrained model
     basis_inputs = Input(shape=(3, HEIGHT, WIDTH, CHANNELS))
-    basis_model = Xception(include_top=False, input_shape=(HEIGHT, WIDTH, CHANNELS))
+    basis_model = MobileNetV2(include_top=False, input_shape=(HEIGHT, WIDTH, CHANNELS))
     # Do not train pretrained model
     basis_model.trainable = False
 
@@ -91,8 +91,6 @@ def create_model(num_embeddings):
 
     # Siamese model
     siamese_model = Model(inputs=basis_inputs, outputs=embeddings)
-    # siamese_model.summary()
-
     return siamese_model
 
 def create_prediction_model(model):
@@ -114,8 +112,8 @@ def accuracy(_,embeddings):
 if __name__ == '__main__':
     # Load data
     create_split_data()
-    train_data = load_dataset(HANDOUT_PATH + 'train_data.txt')
-    val_data = load_dataset(HANDOUT_PATH + 'val_data.txt')
+    train_data = load_dataset(HANDOUT_PATH + 'train_data.txt', augmenting=True)
+    val_data = load_dataset(HANDOUT_PATH + 'val_data.txt', augmenting=False)
 
     # Number of training and test data
     with open(HANDOUT_PATH + 'train_data.txt', 'r') as file:
@@ -123,8 +121,15 @@ if __name__ == '__main__':
     with open(HANDOUT_PATH + 'test_triplets.txt', 'r') as file:
         num_test_data = sum(1 for line in file)
 
-    # Create model
-    model = create_model(NUM_EMBEDDINGS)
+    # Create and compile model
+    if not USE_TRAINED:
+        model = create_model(NUM_EMBEDDINGS)
+    else:
+        with open(HANDOUT_PATH + 'model.json', 'r') as json_file:
+            loaded_model_json = json_file.read()
+        model = tf.keras.models.model_from_json(loaded_model_json)
+        model.load_weights(HANDOUT_PATH + 'model.h5')
+
     model.compile(optimizer=optimizers.Adam(learning_rate=LEARNING_RATE), loss=triplet_loss, metrics=[accuracy])
 
     # Load batches
@@ -133,13 +138,19 @@ if __name__ == '__main__':
 
     # Train model
     model.fit(train_data, steps_per_epoch=int(np.ceil(num_train_data / TRAIN_BATCH_SIZE)),
-              epochs=EPOCHS,validation_data=val_data,validation_steps=10)
+              epochs=EPOCHS,validation_data=val_data,validation_steps=VALIDATION_STEPS)
 
     # Predict labels
-    test_data = load_dataset(HANDOUT_PATH + 'test_triplets.txt')
+    test_data = load_dataset(HANDOUT_PATH + 'test_triplets.txt', augmenting=False)
     test_data = test_data.batch(TEST_BATCH_SIZE).prefetch(2)
     prediction_model = create_prediction_model(model)
-    predictions = prediction_model.predict(test_data,steps=int(np.ceil(num_test_data / TEST_BATCH_SIZE)),verbose=1)
+    predictions = prediction_model.predict(test_data,steps=int(np.ceil(num_test_data / TEST_BATCH_SIZE)))
 
-    np.savetxt('predictions.txt', predictions, fmt='%i')
+    # Save model and weights to disk
+    model_json = model.to_json()
+    with open(HANDOUT_PATH + 'model.json', 'w') as json_file:
+        json_file.write(model_json)
+    model.save_weights(HANDOUT_PATH + 'model.h5')
+
+    np.savetxt('predictions4.txt', predictions, fmt='%i')
     print("Finished!")
